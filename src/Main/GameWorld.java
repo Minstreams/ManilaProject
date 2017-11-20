1f189d7bfc381947515a0fc09aa044f392141685
Minstreams <minstreams@qq.com>
2017-11-20 19:31:43 +0800

    分离test 改进delaySeconds方法

--- src/Main/GameWorld.java
+++ src/Main/GameWorld.java
@@ -1,44 +1,24 @@
 package Main;
 
 import java.util.Calendar;
+import java.util.TimerTask;
 
-import Components.*;
-import Systems.*;
-
-public class GameWorld {
+public class GameWorld extends TimerTask{
 	public static int SystemNum = 0;
 	private void GameLoop() {
 		
 	}
-	public static void main(String[] args) {
-		GameView gameView = new GameView();
-		gameView.CreatView();
-		GameObject g = new GameObject(10,10);
-		g.AddComponent(new RenderComponent("g1.jpg"));
-		g.AddComponent(new ExampleComponent());
-		GameObject g2 = new GameObject(100,100);
-		g2.AddComponent(new RenderComponent("g2.jpg"));
-		RenderSystem.Function();
-		ExampleSystem.Function();
-		InputSystem.Function();
-		gameView.PaintGameObject(g);
-		gameView.PaintGameObject(g2);
-		delaySeconds(1);
-		g2.GetComponent(RenderComponent.class).ChangePos(g2.x, g2.y);
-	}
 
-	public static void delaySeconds(int s) {
-		int o = nowSecond();
-		while(nowSecond()-o <= s) {
+	public static void delaySeconds(float s) {
+		long o = System.currentTimeMillis();
+		long delta = (long)s*1000;
+		while(System.currentTimeMillis()-o <= delta) {
 			
 		}
-		
 	}
-	
-	private static int nowSecond() {
-		int second = 0;
-		Calendar c = Calendar.getInstance();
-		second = c.get(Calendar.SECOND) + c.get(Calendar.MINUTE)*60;
-		return second;
+
+	@Override
+	public void run() {
+		GameLoop();
 	}
 }
