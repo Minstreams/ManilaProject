c5120d1dfbcad231aff9335554ee73ecea22d37a
Minstreams <minstreams@qq.com>
2017-11-22 21:03:26 +0800

    更新KeyCode表

--- src/Main/KeyCode.java
+++ src/Main/KeyCode.java
@@ -1,5 +1,59 @@
 package Main;
 
-public enum KeyCode {
+public class KeyCode {
+    public static int num0 = 48;
+    public static int num1 = 49;
+    public static int num2 = 50;
+    public static int num3 = 51;
+    public static int num4 = 52;
+    public static int num5 = 53;
+    public static int num6 = 54;
+    public static int num7 = 55;
+    public static int num8 = 56;
+    public static int num9 = 57;
+
+    public static int A = 65;
+    public static int B = 66;
+    public static int C = 67;
+    public static int D = 68;
+    public static int E = 69;
+    public static int F = 70;
+    public static int G = 71;
+    public static int H = 72;
+    public static int I = 73;
+    public static int J = 74;
+    public static int K = 75;
+    public static int L = 76;
+    public static int M = 77;
+    public static int N = 78;
+    public static int O = 79;
+    public static int P = 80;
+    public static int Q = 81;
+    public static int R = 82;
+    public static int S = 83;
+    public static int T = 84;
+    public static int U = 85;
+    public static int V = 86;
+    public static int W = 87;
+    public static int X = 88;
+    public static int Y = 89;
+    public static int Z = 90;
+
+    public static int Backspace = 8;
+    public static int Tab = 9;
+    public static int Enter = 10;
+    public static int Shift = 16;
+    public static int Ctrl = 17;
+    public static int Alt = 18;
+    public static int CapsLock = 20;
+    public static int Space = 32;
+    public static int Windows = 524;
+
+    public static int Left = 37;
+    public static int Up = 38;
+    public static int Right = 39;
+    public static int Down = 40;
+
+
 
 }
