1e1188fffad23074872694efb8c4114629e4cf9b
Minstreams <minstreams@qq.com>
2017-12-07 23:12:35 +0800

    菜单场景

--- src/Scenes/StartScene.java
+++ src/Scenes/StartScene.java
@@ -4,22 +4,16 @@ import Components.*;
 import Main.GameObject;
 import Main.Scene;
 
+/**
+ * 菜单场景
+ */
 public class StartScene extends Scene {
 
     @Override
     protected void LoadScene() {
         //菜单场景
-        //AddGameObject(new GameObject("sin",0,0,0));
-        //AddComponent(new RenderComponent("boat.jpg"));
-        //AddComponent(new StartMenuComponent());
-
-        //游戏场景
-        AddGameObject(new GameObject("BackGround",0,0,0));
-        AddComponent(new RenderComponent("sea.gif"));
-        //船儿
-        AddGameObject(new GameObject("Boat",10,60,1));
-        AddComponent(new RenderComponent("boat.gif"));
-        //控制类组件
-        AddComponent(new ControllerComponent(2));
+        AddGameObject(new GameObject("StartScene",0,0,0));
+        AddComponent(new RenderComponent("boat.jpg"));
+        AddComponent(new StartMenuComponent());
     }
 }
