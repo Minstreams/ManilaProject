a94ac7fdd5b698d20337e4b245e39c8b9a2de034
chiellini <18319277690@163.com>
2017-12-07 23:30:52 +0800

    scene 1 layer update

--- src/Scenes/Scene1.java
+++ src/Scenes/Scene1.java
@@ -7,7 +7,21 @@ import Main.Scene;
 public class Scene1 extends Scene {
 
     @Override
-    protected void LoadScene() {
+    protected void LoadScene()
+    {
 
+        //游戏场景
+        AddGameObject(new GameObject("BackGround",0,0,0));
+        AddComponent(new RenderComponent("sea.gif"));
+        //船儿
+        AddGameObject(new GameObject("Boat",10,60,1));
+        AddComponent(new RenderComponent("boat.gif"));
+        //控制类组件
+        AddComponent(new BoatControllerComponent());
+
+        //产生石头,在该物体里面持续产生
+        AddGameObject(new GameObject("Rocks",0,0,2));
+        //在该组件里面给物体产生子物体 即炸弹
+        AddComponent(new RocksCreatAndDestoryComponent());
     }
 }
