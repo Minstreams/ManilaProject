6b7307f80979b72c6133c79b70b4d378749e88ec
junji2016 <fy3277612@hotmail.com>
2017-12-08 16:42:55 +0800

    随便设置了三个关卡

--- src/Scenes/Scene2.java
+++ src/Scenes/Scene2.java
@@ -4,7 +4,7 @@ import Components.*;
 import Main.GameObject;
 import Main.Scene;
 
-public class Scene1 extends Scene {
+public class Scene2 extends Scene {
 
     @Override
     protected void LoadScene() {
@@ -19,12 +19,12 @@ public class Scene1 extends Scene {
         //控制类组件
         AddComponent(new BoatControllerComponent(150));
         //计分组件
-        AddComponent(new PointCountComponent(1000));
+        AddComponent(new PointCountComponent(2000));
 
         //产生石头,在该物体里面持续产生
         AddGameObject(new GameObject("Rocks", 0, 0, 2));
         //在该组件里面给物体产生子物体 即炸弹
-        AddComponent(new RocksCreatAndDestoryComponent(1f,"rock.jpg",50,0));
+        AddComponent(new RocksCreatAndDestoryComponent(0.75f,"rock.jpg",70,0));
 
         //开火组件
         Find("Boat").AddComponent(new FireComponent(Find("Rocks")));
