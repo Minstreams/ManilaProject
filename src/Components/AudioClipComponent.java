b503e933c96a36041fb2d61c831faf316f448fd0
Minstreams <minstreams@qq.com>
2017-12-08 12:15:50 +0800

    添加声音组件

---
+++ src/Components/AudioClipComponent.java
@@ -0,0 +1,48 @@
+package Components;
+
+import Systems.ComponentSystem;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 声音组件
+ */
+public class AudioClipComponent extends ComponentSystem {
+    private AudioStream audioStream;//声音数据流
+    private boolean playOnStart = false;//是否开始就播放
+
+    public AudioClipComponent(String audioName, boolean playOnStart) {
+        this.playOnStart = playOnStart;
+        try {
+            audioStream = new AudioStream(new FileInputStream(System.getProperty("user.dir") + "\\src\\" + audioName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void function() {
+
+    }
+
+    @Override
+    public void start() {
+        if (playOnStart) {
+            Play();
+        }
+    }
+
+    public void Play() {
+        AudioPlayer.player.start(audioStream);
+    }
+
+    public void Stop() {
+        AudioPlayer.player.stop(audioStream);
+    }
+}
