be47b093e841c69dc8dc1ce278c9cbabf2b2b76e
Minstreams <minstreams@qq.com>
2017-12-08 14:22:49 +0800

    进阶成就

--- src/Components/AchivementComponent.java
+++ src/Components/AchivementComponent.java
@@ -1,11 +1,38 @@
 package Components;
 
 import Main.Achievements;
+import Main.GameObject;
+import Systems.ComponentSystem;
 
-public class AchivementComponent {
+public class AchivementComponent extends ComponentSystem {
+
+    private static int startY = 160;
+    private static int deltaY = 30;
+    private static float moveRate;
+
+    protected static int boardNum = 0;
 
     public static void ShowAchivement(Achievements achievement) {
         //TODO
         System.out.println("获得" + achievement.name() + "!");
+        GameObject g = gameWorld.getCurrentScene().AddGameObject(new GameObject("Achievement Board", 959, startY + deltaY * boardNum));
+        g.AddComponent(new AchivementComponent());
+        g.AddComponent(new RenderComponent("achievementBoard.jpg"));
+    }
+
+    @Override
+    protected void function() {
+        gameObject.x += (800 - gameObject.x) * moveRate;
+    }
+
+    @Override
+    public void start() {
+        boardNum++;
+    }
+
+    @Override
+    public void OnDestroy() {
+        super.OnDestroy();
+        boardNum--;
     }
 }
